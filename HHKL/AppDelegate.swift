a9cb5df7a3c9198dc8341c2c5fbc35a4e0a0bfe8
Alexey Korolev <alphatroya@gmail.com>
2016-01-27 14:30:30 +0300

    Add Carthage libraries

--- HHKL/AppDelegate.swift
+++ HHKL/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
